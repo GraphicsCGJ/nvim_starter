68a8d7d104c3a281b9b63361c85eea3871d33a25
gyujin_choi <ckjin95@gmail.com>
2025-07-20 19:40:32 +0900

    Title: zz
    Descriptions: zz

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -8,20 +8,57 @@ local servers = {
   "clangd",
 }
 
+-- lspconfig.pyright.setup {
+--   on_attach = on_attach,
+--   settings = 
+--   {
+--     pyright = {
+--       autoImportCompletion = true,
+--     },
+--     python = {
+--       analysis = 
+--         autoSearchPaths = true,diagnosticMode = 'openFilesOnly',useLibraryCodeForTypes = true,typeCheckingMode = 'off'
+--     }
+--   }
+-- }
+
+--
+-- vim.lsp.config("pyright", {
+--   settings = {
+--     python = {
+--       analysis = {
+--         typeCheckingMode = "strict",
+--       },
+--     },
+--   },
+--   root_dir = function(fname)
+--     return require("lspconfig.util").root_pattern("pyproject.toml", "setup.py")(fname)
+--       or vim.fn.getcwd()
+--   end,
+-- })
+--
+--
 vim.lsp.enable(servers)
 
-
-vim.lsp.config("pyright", {
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "strict",
-      },
-    },
-  },
+require('lspconfig').pyright.setup{
   root_dir = function(fname)
-    return require("lspconfig.util").root_pattern("pyproject.toml", "setup.py")(fname)
+    return require('lspconfig.util').root_pattern('setup.py', 'pyproject.toml')(fname)
       or vim.fn.getcwd()
   end,
-})
+  on_attach = on_attach,
+  settings = 
+    {
+      pyright = {
+        autoImportCompletion = true,
+      },
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          diagnosticMode = 'openFilesOnly',
+          useLibraryCodeForTypes = true,
+          typeCheckingMode = 'off'
+        }
+      }
+    }
+}
 
