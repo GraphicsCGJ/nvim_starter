3cdae17735e367710a420cc965f8fff608684ce4
gyujin.choi <ckjin95@gmail.com>
2025-12-08 00:44:14 +0900

    [LSP] Fixed unused lsp files to use it
    Changed:
    * Copied lsp config files into lua/lsp files
    * Added vim.lsp.config to config lsp conf files
    
    Signed-off-by: gyujin.choi <ckjin95@gmail.com>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
+vim.lsp.config('pyright', require("lsp.pyright"))
 vim.lsp.enable('pyright')
+vim.lsp.config("eslint", require("lsp.eslint"))
 vim.lsp.enable('eslint')
+vim.lsp.config('clangd', require("lsp.clangd"))
 vim.lsp.enable('clangd')
 vim.lsp.enable('solidity_ls_nomicfoundation')
 vim.lsp.enable('mesonlsp')
